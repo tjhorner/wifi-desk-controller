bb329b35c2b4a3a5fe1fc9337e1e68fc2204b8ca
TJ Horner <me@tjhorner.com>
2022-05-15 23:56:42 -0400

    macos cringe

--- firmware/make-stock-bin.sh
+++ firmware/make-stock-bin.sh
@@ -5,7 +5,7 @@ mkdir -p build bin
 
 touch build/standing-desk.yaml
 cat config.yaml >> build/standing-desk.yaml
-sed -i "" "s/DESK_BRAND/$1/" build/standing-desk.yaml
+sed -i "s/DESK_BRAND/$1/" build/standing-desk.yaml
 
 esphome compile build/standing-desk.yaml
 cp build/.esphome/build/standing-desk/.pioenvs/standing-desk/firmware.bin bin/stock-$1.bin
